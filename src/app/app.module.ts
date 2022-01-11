265343332c64744ad761a337a79a86eca4360a4e
Muhammad Hammad <muhammadhammad366@gmail.com>
2022-01-11 12:39:22 +0500

    edit app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-
+// I've changed branch mHammad
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
