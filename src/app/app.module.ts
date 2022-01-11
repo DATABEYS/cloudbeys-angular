c501ed19e800cc9520612adcf0e74849a5735089
Muhammad Hammad <muhammadhammad366@gmail.com>
2022-01-11 12:45:47 +0500

    main merged in hammad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// I've changed branch mHammad
-=======
 // I've changed it
->>>>>>> main
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
